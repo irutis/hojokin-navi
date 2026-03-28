b6e096fb5e87e6aac44635128bbdc3164965e9fa
irutis <ta1morim3222@gmail.com>
2026-03-27 23:30:56 -1000

    Add Google AdSense code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,13 @@ export default function RootLayout({
       lang="ja"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2317215173633118"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
